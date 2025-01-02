f885dfdfbab17b478636b891d7ea0a1d88f5efa4
MM004 <mayrguendter.mathias@gmx.at>
2025-01-02 09:38:02 +0100

    add csv donwload for single atms except sCTMP and cATMP

--- Database/downloads.py
+++ Database/downloads.py
@@ -10,19 +10,20 @@ all_dfs = joblib.load('../all_dfs.pkl')
 st.title('Download Database')
 
 options = st.selectbox(
-    "Options",
+    'Options',
     options=('All', 'GTMP', 'TEP', 'sCTMP', 'cATMP'),
     index=None,
-    placeholder="Choose an Option"
+    placeholder='Choose an Option'
 )
 
 formats = st.selectbox(
-    "Format",
+    'Format',
     options=('CSV', 'Excel'),
     index=None,
-    placeholder="Choose an Option"
+    placeholder='Choose an Option'
 )
 
+# Donwload all ATMPS as CSV 
 if formats == 'CSV' and options == 'All':
     # create dirs with all atmps
     if not os.path.isdir('all_dfs_csv'):
@@ -34,16 +35,33 @@ if formats == 'CSV' and options == 'All':
             all_dfs[category][atmp].to_csv(f'all_dfs_csv/{category}/{atmp}.csv', index=False)
     # create Zip
     shutil.make_archive('all_dfs_csv'.replace('.zip', ''), 'zip', 'all_dfs_csv')
-    
     # Donwload zip archive
     with open('all_dfs_csv.zip', 'rb') as f:
         btn_all_dfs_csv = st.download_button(
-            label = "Download all atmps as CSV!",
+            label = 'Download all ATMPs as CSV!',
             data = f.read(),
-            file_name ='all_dfs_csv.zip',
+            file_name = 'all_dfs_csv.zip',
             mime ='application/zip'
     )
-    
+# Donwload single ATMPS as CSV 
+elif formats == 'CSV' and options != 'All' and options != 'sCTMP' and options != 'cATMP':
+    # create dir for selected atmp
+    for cat in all_dfs.keys():
+        if not os.path.isdir(cat):
+            os.makedirs(cat)
+        for atmp in all_dfs[cat]:
+            all_dfs[cat][atmp].to_csv(f'{cat}/{atmp}.csv', index=False)
+        # create Zip
+        shutil.make_archive(cat.replace('.zip', ''), 'zip', cat)
+        # Donwload zip archive
+        if options == cat:
+            with open(f'{cat}.zip', 'rb') as f:
+                btn_all_dfs_csv = st.download_button(
+                    label = f'Download all {cat} as CSV!',
+                    data = f.read(),
+                    file_name = f'{cat}.zip',
+                    mime ='application/zip'
+            )
 else:
     st.write('Not implemented yet!')
     
