abb4d5a110e78a6ee4078ec64a7e3500375a2de1
MM004 <mayrguendter.mathias@gmx.at>
2025-01-02 10:41:34 +0100

    implment excel download

--- Database/downloads.py
+++ Database/downloads.py
@@ -23,45 +23,93 @@ formats = st.selectbox(
     placeholder='Choose an Option'
 )
 
-# Donwload all ATMPS as CSV 
+"""
+Create Download Files and download options for CSV and Excel
+"""
+# CSV
+## Donwload all ATMPS as CSV 
 if formats == 'CSV' and options == 'All':
     # create dirs with all atmps
-    if not os.path.isdir('all_dfs_csv'):
-        os.makedirs('all_dfs_csv')
+    if not os.path.isdir('downloads_csv/all_dfs_csv'):
+        os.makedirs('downloads_csv/all_dfs_csv')
     for category in all_dfs.keys():
         for atmp in all_dfs[category]:
-            if not os.path.isdir(f'all_dfs_csv/{category}'):
-                os.makedirs(f'all_dfs_csv/{category}')
-            all_dfs[category][atmp].to_csv(f'all_dfs_csv/{category}/{atmp}.csv', index=False)
+            if not os.path.isdir(f'downloads_csv/all_dfs_csv/{category}'):
+                os.makedirs(f'downloads_csv/all_dfs_csv/{category}')
+            all_dfs[category][atmp].to_csv(f'downloads_csv/all_dfs_csv/{category}/{atmp}.csv', index=False)
     # create Zip
-    shutil.make_archive('all_dfs_csv'.replace('.zip', ''), 'zip', 'all_dfs_csv')
+    shutil.make_archive('downloads_csv/all_dfs_csv'.replace('.zip', ''), 'zip', 'downloads_csv/all_dfs_csv')
     # Donwload zip archive
-    with open('all_dfs_csv.zip', 'rb') as f:
+    with open('downloads_csv/all_dfs_csv.zip', 'rb') as f:
         btn_all_dfs_csv = st.download_button(
             label = 'Download all ATMPs as CSV!',
             data = f.read(),
             file_name = 'all_dfs_csv.zip',
             mime ='application/zip'
     )
-# Donwload single ATMPS as CSV 
+## Donwload single ATMPS as CSV 
 elif formats == 'CSV' and options != 'All' and options != 'sCTMP' and options != 'cATMP':
     # create dir for selected atmp
     for cat in all_dfs.keys():
-        if not os.path.isdir(cat):
-            os.makedirs(cat)
+        if not os.path.isdir(f'downloads_csv/{cat}'):
+            os.makedirs(f'downloads_csv/{cat}')
         for atmp in all_dfs[cat]:
-            all_dfs[cat][atmp].to_csv(f'{cat}/{atmp}.csv', index=False)
+            all_dfs[cat][atmp].to_csv(f'downloads_csv/{cat}/{atmp}.csv', index=False)
         # create Zip
-        shutil.make_archive(cat.replace('.zip', ''), 'zip', cat)
+        shutil.make_archive(f'downloads_csv/{cat}'.replace('.zip', ''), 'zip', f'downloads_csv/{cat}')
         # Donwload zip archive
         if options == cat:
-            with open(f'{cat}.zip', 'rb') as f:
+            with open(f'downloads_csv/{cat}.zip', 'rb') as f:
                 btn_all_dfs_csv = st.download_button(
                     label = f'Download all {cat}s as CSV!',
                     data = f.read(),
                     file_name = f'{cat}.zip',
                     mime ='application/zip'
             )
+
+# Excel
+## Donwload all ATMPS as Excel 
+if formats == 'Excel' and options == 'All':
+    # create dirs with all atmps
+    if not os.path.isdir('downloads_excel/all_dfs_excel'):
+        os.makedirs('downloads_excel/all_dfs_excel')
+    for category in all_dfs.keys():
+        for atmp in all_dfs[category]:
+            if not os.path.isdir(f'downloads_excel/all_dfs_excel/{category}'):
+                os.makedirs(f'downloads_excel/all_dfs_excel/{category}')
+            # create Excels
+            all_dfs[category][atmp].to_excel(f'downloads_excel/all_dfs_excel/{category}/{atmp}.xlsx', index=False)
+    # create Zip
+    shutil.make_archive('downloads_excel/all_dfs_excel'.replace('.zip', ''), 'zip', 'downloads_excel/all_dfs_excel')
+    # Donwload zip archive
+    with open('downloads_excel/all_dfs_excel.zip', 'rb') as f:
+        btn_all_dfs_excel = st.download_button(
+            label = 'Download all ATMPs as Excel!',
+            data = f.read(),
+            file_name = 'all_dfs_excel.zip',
+            mime ='application/zip'
+    )
+## Donwload single ATMPS as Excel 
+elif formats == 'Excel' and options != 'All' and options != 'sCTMP' and options != 'cATMP':
+    # create dir for selected atmp
+    for cat in all_dfs.keys():
+        if not os.path.isdir(f'downloads_excel/{cat}'):
+            os.makedirs(f'downloads_excel/{cat}')
+        for atmp in all_dfs[cat]:
+            all_dfs[cat][atmp].to_excel(f'downloads_excel/{cat}/{atmp}.xlsx', index=False)
+        # create Zip
+        shutil.make_archive(f'downloads_excel/{cat}'.replace('.zip', ''), 'zip', f'downloads_excel/{cat}')
+        # Donwload zip archive
+        if options == cat:
+            with open(f'downloads_excel/{cat}.zip', 'rb') as f:
+                btn_all_dfs_excel = st.download_button(
+                    label = f'Download all {cat}s as Excel!',
+                    data = f.read(),
+                    file_name = f'{cat}.zip',
+                    mime ='application/zip'
+            )
+                
+# Not implmented yet!
 else:
     st.write('Not implemented yet!')
     
