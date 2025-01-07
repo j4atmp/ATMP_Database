2f8f2b6f50ca3d03fe0d13f978721a891f5ffc0d
MM004 <mayrguendter.mathias@gmx.at>
2025-01-07 08:35:25 +0100

    fix bug

--- dataframe_conversion.py
+++ dataframe_conversion.py
@@ -19,7 +19,7 @@ def df_converter():
             tmp = pd.read_excel(f'{category}/{file}', skiprows=(0, 14, 35), header=None, index_col=0)
             tmp = tmp.T
             tmp = tmp.drop(columns=np.nan)
-            atmp_id = tmp['Title of ATMP / ATMP identifyer / Investigational medicinal product name'].iloc[0]
+            atmp_id = tmp[tmp.columns[0]].iloc[0]
             if atmp_id in all_dfs[category].keys():
                 continue
             else:
