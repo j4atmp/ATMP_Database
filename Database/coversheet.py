493c406c2458b93369ca804fca9cb5b65a1388f6
MM004 <mayrguendter.mathias@gmx.at>
2025-01-20 12:08:54 +0100

    add comments and fix mistake

--- Database/coversheet.py
+++ Database/coversheet.py
@@ -36,33 +36,56 @@ def coversheet_creator(all_dfs, category, atmp):
     tab1, tab2, tab3 = st.tabs(['ATMP Cover Sheet', 'Regulatory Information', 'WP 1'])
 
     with tab1:
+        # Tab for ATMP Cover Sheet
         st.subheader('ATMP Cover Sheet')
-        test = all_dfs[category][atmp].iloc[:,0:12].T
-        
-        test = test.reset_index()
-        test.rename(columns={index : str(value) for index,value in enumerate(range(len(test.columns))) }, inplace=True)
-        test.rename(columns={'0': 'Fields', 1: 'Values'}, inplace=True)
-        test = test.dropna(axis=1,how='all')
-        test.replace(np.nan, '', inplace=True)
-        st.dataframe(test, hide_index=True, use_container_width=True, height=458)
+        # Process rows 2 to 13 in the Exel Sheet and transpose 
+        cs = all_dfs[category][atmp].iloc[:,0:12].T
+        # Set the index to the first row
+        cs = cs.reset_index()
+        # Rename the columns from 0 onwards
+        cs.rename(columns={index : str(value) for index,value in enumerate(range(len(cs.columns))) }, inplace=True)
+        # Rename the first column to 'Fields'
+        cs.rename(columns={'0': 'Fields', 1: 'Values'}, inplace=True)
+        # Drop columns with all NaN values
+        cs = cs.dropna(axis=1,how='all')
+        # Replace NaN values with empty strings
+        cs.replace(np.nan, '', inplace=True)
+        # Display the dataframe
+        st.dataframe(cs, hide_index=True, use_container_width=True, height=458)
 
     with tab2:
+        # Tab for Regulatory Information
         st.subheader('Regulatory Information')
-        test2 = all_dfs[category][atmp].iloc[:,12:30].T
-        test2 = test2.reset_index()
-        test2.rename(columns={index : str(value) for index,value in enumerate(range(len(test2.columns))) }, inplace=True)
-        test2.rename(columns={'0':'Fields' }, inplace=True)
-        test2 = test2.dropna(axis=1,how='all')
-        test2.replace(np.nan, '', inplace=True)
-        st.dataframe(test2, hide_index=True, use_container_width=True, height=668)
+        # Process rows 16 to 34 in the Exel Sheet and transpose 
+        ri = all_dfs[category][atmp].iloc[:,12:31].T
+        # Set the index to the first row
+        ri = ri.reset_index()
+        # Rename the columns from 0 onwards
+        ri.rename(columns={index : str(value) for index,value in enumerate(range(len(ri.columns))) }, inplace=True)
+        # Rename the first column to 'Fields'
+        ri.rename(columns={'0':'Fields' }, inplace=True)
+        # Drop columns with all NaN values
+        ri = ri.dropna(axis=1,how='all')
+        # Replace NaN values with empty strings
+        ri.replace(np.nan, '', inplace=True)
+        # Display the dataframe
+        st.dataframe(ri, hide_index=True, use_container_width=True, height=668)
 
     with tab3:
+        # Tab for WP 1
         st.subheader('WP 1')
-        test3 = all_dfs[category][atmp].iloc[:,31:].T
-        test3 = test3.reset_index()
-        test3.rename(columns={index : str(value) for index,value in enumerate(range(len(test3.columns))) }, inplace=True)
-        test3.rename(columns={'0': 'Fields'}, inplace=True)
-        test3 = test3.dropna(axis=1,how='all')
-        test3.replace(np.nan, '', inplace=True)
-        st.dataframe(test3, hide_index=True, use_container_width=True, height=773)
+        # Process rows 37 to 57 in the Exel Sheet and transpose
+        wp1 = all_dfs[category][atmp].iloc[:,31:].T
+        # Set the index to the first row
+        wp1 = wp1.reset_index()
+        # Rename the columns from 0 onwards
+        wp1.rename(columns={index : str(value) for index,value in enumerate(range(len(wp1.columns))) }, inplace=True)
+        # Rename the first column to 'Fields'
+        wp1.rename(columns={'0': 'Fields'}, inplace=True)
+        # Drop columns with all NaN values
+        wp1 = wp1.dropna(axis=1,how='all')
+        # Replace NaN values with empty strings
+        wp1.replace(np.nan, '', inplace=True)
+        # Display the dataframe
+        st.dataframe(wp1, hide_index=True, use_container_width=True, height=773)
 
--- Database/tep.py
+++ Database/tep.py
@@ -1,21 +1,21 @@
 import streamlit as st
 import joblib
 from coversheet import coversheet_creator
-
+# C
 st.title('TEP')
-
+# load the data
 all_dfs = joblib.load('../all_dfs.pkl')
-
+# get the list of all TEPs
 all_teps= list(all_dfs['TEP'].keys())
 all_teps.sort()
-
+# select the TEP and create selctbox
 option = st.selectbox(
     "TEP List",
     options=all_teps,
     index=None,
     placeholder="Choose an ATMP"
 )
-
+# create the coversheet for the selected TEP
 for i in all_teps:
     if option == i:
         coversheet_creator(all_dfs, category='TEP', atmp=i)
