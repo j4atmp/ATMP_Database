9a73ca285522b88c90d04ff421849a03362b5be6
MM004 <mayrguendter.mathias@gmx.at>
2025-01-22 18:27:50 +0100

    edit message

--- Database/title_page.py
+++ Database/title_page.py
@@ -9,8 +9,8 @@ st.image(path_logo, width=800)
 
 st.link_button("J4ATMP Homepage", 'https://join4atmp.eu/')
 
-st.write("""The JOIN4ATMP project aims to accelerate a
-         nd de-risk European ATMP development and 
+st.write("""The JOIN4ATMP project aims to accelerate and 
+         de-risk European ATMP development and 
          ensure wide-spread access of ATMPs, 
          through the mapping of obstacles to such development, 
          the audit of real-world-based solutions 
