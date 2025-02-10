a82a39914d026aa6067282de6a324133010f2a62
Mathias Mayrgündter <137404180+MM004@users.noreply.github.com>
2025-02-10 13:06:08 +0000

    bug fix

--- Database/pages/title_page.py
+++ Database/pages/title_page.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import os
 
-path_logo = os.path.join(os.getcwd(),"Database", "logo.png")
+path_logo = os.path.join(os.getcwd(),"Database/pages", "logo.png")
 
 st.title('ATMP-Database')
 
