53f790bf8b52aff6b2a761dd6b49ea4e185ddee6
Mathias Mayrgündter <137404180+MM004@users.noreply.github.com>
2025-02-10 16:17:20 +0000

    new upload button

--- Database/pages/uploads.py
+++ Database/pages/uploads.py
@@ -3,27 +3,36 @@ import joblib
 import os
 import pandas as pd
 import numpy as np
+from streamlit_gsheets import GSheetsConnection
 from utilities import check_password
 
+CHUNK_SIZE = 60
+ATMP_CATEGORY = 14 # atmp.iloc[ATMP_CATEGORY][1] == Category_Value
+ATMP_ID = 1  # atmp.iloc[ATMP_ID][1] == ID_Value
 
-path_dataframes = os.path.join(os.getcwd(), "all_dfs.pkl")
-path_coversheets = os.path.join(os.getcwd(), "ATMP Sheets")
+# connect to Master sheet on Google Drive
+conn = st.connection("gsheets", type=GSheetsConnection)
+df_master = conn.read()
+# load the data
+all_dfs = df_master.copy()
+# create chunks with size 60
+all_dfs_chunks = [all_dfs.iloc[i:i + CHUNK_SIZE] for i in range(0, len(all_dfs), CHUNK_SIZE)]
 
 st.title('Upload ATMP-Sheet')
 # load the data
-all_dfs = joblib.load(path_dataframes)
 
 st.markdown('''
-    For uploading ATMP-Sheets you need a User login.   
-    If you have no User Login there are two options:
+    For uploading ATMP-Sheets you need the WP1 User.   
+    If you don't have a WP1 User for login there are two options:
             
-        1. send Email with the ATMP 
-        2. or send Email to register for a user*
+        1. send an Email with the ATMP* 
+        2. or send an Email to register for the WP1 User*
     to j4atmp@gmail.com.
+
+    *In both cases you need to state your affiliation to J4ATMP and Personal Information.
     
     **:red[Please always use the Template Format and do not change it!!!]**
     
-    *here you need to state your affiliation to J4ATMP and Personal Information.
     ''')
 
 
@@ -37,38 +46,52 @@ with open(file, 'rb') as my_file:
     file_name = 'ATMP_Cover_Sheet_Example.xlsx', 
     mime = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')      
 
+template = pd.read_excel(file, header=None)
 
+# create list of current ATMP IDs
+Current_Atmps = set()
+for chunk in all_dfs_chunks:
+    if chunk.iloc[ATMP_ID][1] not in Current_Atmps:
+        Current_Atmps.add(chunk.iloc[ATMP_ID][1])
+
+# Check for WP1 User
 if not check_password():
     st.stop()
 
 
-category = st.selectbox(
-    "Choose ATMP category",
-    options=['GTMP', 'TEP', 'sCTMP','cATMP'],
-    index=None,
-    placeholder="GTMP"
+uploaded_files = st.file_uploader(
+    "Choose an ATMP in the right Format (see Cover Sheet Example)", 
+    accept_multiple_files=True,
+    type = 'xlsx'
 )
-if category is not None:
-    uploaded_files = st.file_uploader(
-        "Choose an ATMP in the right Format (see Cover Sheet Example)", 
-        accept_multiple_files=True,
-        type = 'xlsx'
-    )
 
-    for uploaded_file in uploaded_files:
-        data_upload = pd.read_excel(uploaded_file, skiprows=(0, 14, 35), header=None, index_col=0)
-        data_upload = data_upload.T
-        data_upload = data_upload.drop(columns=np.nan)
-        adata_upload_id = data_upload[data_upload.columns[0]].iloc[0]
-        if category in all_dfs.keys():
-            continue
+new_df = all_dfs.copy()
+tmp_dfs = []
+
+for uploaded_file in uploaded_files:
+    data_upload = pd.read_excel(uploaded_file, header=None)
+
+    # check if the ATMPs are already in the master file
+    if data_upload.iloc[ATMP_ID][1] not in Current_Atmps:
+        # check if all fileds are the same and in the same order as in the template
+        if list(data_upload[0]) == list(template[0]):
+            # load uploaded files into tmp list
+            tmp_dfs.append(data_upload)
+            st.markdown(f'**:green[{uploaded_file.name}]** no Errors found!')
         else:
-            all_dfs[category] = dict()
-        all_dfs[category][adata_upload_id] = data_upload
+            st.markdown(f'File format for **:red[{uploaded_file.name}]** is **not correct**! Please check with Cover Sheet Example!')  
+    else:
+        st.markdown(f'ATMP **:red[{uploaded_file.name}]** already exists!')     
 
-        joblib.dump(all_dfs, 'all_dfs.pkl')
-        
-
-
-          
-          
+# check if there are files with correct format 
+if len(tmp_dfs) > 0:
+    upload_button = st.button('Upload all correct ATMP-Files!')  
+    if upload_button:
+        for new_atmp_file in tmp_dfs:
+            # change colums of new_atmp_df to match current columns
+            new_atmp_file.columns = all_dfs.columns
+            # append new_atmp_df to current atmp_df
+            new_df = pd.concat([new_df, new_atmp_file], ignore_index=True)
+        # update masterfile
+        conn.update(data=new_df)
+        st.write("Upload successful!")
