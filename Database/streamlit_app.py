39634cc75bf385131443dc4cc7e12e2653ee0865
MM004 <mayrguendter.mathias@gmx.at>
2025-03-10 11:15:48 +0100

    reorganize and add dashboard

--- Database/streamlit_app.py
+++ Database/streamlit_app.py
@@ -1,18 +1,26 @@
 import streamlit as st
 
-title_page = st.Page("pages/title_page.py", title="Home", icon=":material/home:")
-
-# pages
-gtmp = st.Page("pages/gtmp.py", title="GTMP", icon=":material/dataset:")
-tep = st.Page("pages/tep.py", title="TEP", icon=":material/dataset:")
-sCTMP = st.Page("pages/sCTMP.py", title="sCTMP", icon=":material/dataset:")
-cATMP = st.Page("pages/cATMP.py", title="cATMP", icon=":material/dataset:")
-# Divers
-search_page = st.Page("pages/search_page.py", title="Search", icon=":material/search:")
-download_page = st.Page("pages/downloads.py", title="Download", icon=":material/download:")
-help_page = st.Page("pages/help_page.py", title="Help", icon=":material/help:")
-upload_page = st.Page("pages/uploads.py", title="Upload", icon=":material/upload:")
-
-pg = st.navigation([title_page, gtmp, tep, sCTMP, cATMP, search_page, download_page,upload_page, help_page])
 st.set_page_config(layout="wide",page_title="ATMP Database", page_icon=":material/menu:")
+
+
+title_page = st.Page("pages/title_page.py", title="Home", icon=":material/home:")
+help_page = st.Page("pages/help_page.py", title="Help", icon=":material/help:")
+# atmps
+dashboard = st.Page("pages/atmps/dashboard.py", title="Dashboard", icon=":material/toolbar:")
+gtmp = st.Page("pages/atmps/gtmp.py", title="GTMP", icon=":material/dataset:")
+tep = st.Page("pages/atmps/tep.py", title="TEP", icon=":material/dataset:")
+sCTMP = st.Page("pages/atmps/sCTMP.py", title="sCTMP", icon=":material/dataset:")
+cATMP = st.Page("pages/atmps/cATMP.py", title="cATMP", icon=":material/dataset:")
+# tools
+search_page = st.Page("pages/tools/search_page.py", title="Search", icon=":material/search:")
+download_page = st.Page("pages/tools/downloads.py", title="Download", icon=":material/download:")
+upload_page = st.Page("pages/tools/uploads.py", title="Upload", icon=":material/upload:")
+
+pg = st.navigation(
+    {
+    'Home' : [title_page,help_page],
+    'ATMPs' : [dashboard, gtmp, tep, sCTMP, cATMP],
+    'Tools': [search_page, download_page,upload_page]
+    }
+)
 pg.run()
